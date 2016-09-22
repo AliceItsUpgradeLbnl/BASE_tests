b18f25a6bed0bc8018ef6e63db86d6863eb13b0a
MP <mploskon@lbl.gov>
2016-09-22 12:59:32 -0700

    ALPIDE fx3 and Latchup software

---
+++ ITS_BASEtest_Sept16/ALPIDEtest/Latchup/ALPIDE4/Monitor_C.d
@@ -0,0 +1,66 @@
+
+# DO NOT DELETE
+
+./Monitor_C.so: /home/its/root/include/TStopwatch.h
+./Monitor_C.so: /home/its/root/include/TObject.h
+./Monitor_C.so: /home/its/root/include/Rtypes.h
+./Monitor_C.so: /home/its/root/include/RConfig.h
+./Monitor_C.so: /home/its/root/include/RVersion.h
+./Monitor_C.so: /home/its/root/include/DllImport.h
+./Monitor_C.so: /home/its/root/include/Rtypeinfo.h
+./Monitor_C.so: /home/its/root/include/snprintf.h
+./Monitor_C.so: /home/its/root/include/strlcpy.h
+./Monitor_C.so: /home/its/root/include/TGenericClassInfo.h
+./Monitor_C.so: /home/its/root/include/TSchemaHelper.h
+./Monitor_C.so: /home/its/root/include/TStorage.h
+./Monitor_C.so: /home/its/root/include/TVersionCheck.h
+./Monitor_C.so: /home/its/root/include/Riosfwd.h
+./Monitor_C.so: /home/its/root/include/TBuffer.h
+./Monitor_C.so: /home/its/root/include/TCanvas.h
+./Monitor_C.so: /home/its/root/include/TPad.h
+./Monitor_C.so: /home/its/root/include/TVirtualPad.h
+./Monitor_C.so: /home/its/root/include/TAttPad.h
+./Monitor_C.so: /home/its/root/include/TVirtualX.h
+./Monitor_C.so: /home/its/root/include/TNamed.h
+./Monitor_C.so: /home/its/root/include/TString.h
+./Monitor_C.so: /home/its/root/include/TMathBase.h
+./Monitor_C.so: /home/its/root/include/TAttLine.h
+./Monitor_C.so: /home/its/root/include/TAttFill.h
+./Monitor_C.so: /home/its/root/include/TAttText.h
+./Monitor_C.so: /home/its/root/include/TAttMarker.h
+./Monitor_C.so: /home/its/root/include/GuiTypes.h
+./Monitor_C.so: /home/its/root/include/Buttons.h
+./Monitor_C.so: /home/its/root/include/TQObject.h
+./Monitor_C.so: /home/its/root/include/TAttBBox2D.h
+./Monitor_C.so: /home/its/root/include/TPoint.h
+./Monitor_C.so: /home/its/root/include/TAttCanvas.h
+./Monitor_C.so: /home/its/root/include/TCanvasImp.h
+./Monitor_C.so: /home/its/root/include/TLegend.h
+./Monitor_C.so: /home/its/root/include/TPave.h /home/its/root/include/TBox.h
+./Monitor_C.so: /home/its/root/include/TLatex.h
+./Monitor_C.so: /home/its/root/include/TText.h
+./Monitor_C.so: /home/its/root/include/TGraph.h
+./Monitor_C.so: /home/its/root/include/TVectorFfwd.h
+./Monitor_C.so: /home/its/root/include/TVectorDfwd.h
+./Monitor_C.so: /home/its/root/include/TFitResultPtr.h
+./Monitor_C.so: /home/its/root/include/TMultiGraph.h
+./Monitor_C.so: /home/its/root/include/TF1.h
+./Monitor_C.so: /home/its/root/include/TFormula.h
+./Monitor_C.so: /home/its/root/include/TBits.h
+./Monitor_C.so: /home/its/root/include/TObjArray.h
+./Monitor_C.so: /home/its/root/include/TSeqCollection.h
+./Monitor_C.so: /home/its/root/include/TCollection.h
+./Monitor_C.so: /home/its/root/include/TIterator.h
+./Monitor_C.so: /home/its/root/include/Math/ParamFunctor.h
+./Monitor_C.so: /home/its/root/include/TGaxis.h
+./Monitor_C.so: /home/its/root/include/TLine.h /home/its/root/include/TAxis.h
+./Monitor_C.so: /home/its/root/include/TAttAxis.h
+./Monitor_C.so: /home/its/root/include/TArrayD.h
+./Monitor_C.so: /home/its/root/include/TArray.h
+./Monitor_C.so: /home/its/root/include/TROOT.h
+./Monitor_C.so: /home/its/root/include/TDirectory.h
+./Monitor_C.so: /home/its/root/include/TList.h
+./Monitor_C.so: /home/its/root/include/TDatime.h
+./Monitor_C.so: /home/its/root/include/TUUID.h
+./Monitor_C.so: /home/its/root/include/cintdictversion.h /home/its/root/include/RVersion.h
+Monitor_C__ROOTBUILDVERSION= 5.34/34
